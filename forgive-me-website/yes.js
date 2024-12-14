0185b117eb943817ed28f65035f3bdeb5d3d5d6d
forgive-me-m <alittleone33@gmail.com>
2024-12-14 23:30:13 +0300

    Delete forgive-me-website directory

--- forgive-me-website/yes.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    setTimeout(function() {
-        document.getElementById("delayed-text").style.display = "block";
-        document.getElementById("delayed-text").style.opacity = 1;
-    }, 5000); // 5000 milisaniye = 5 saniye
-};
